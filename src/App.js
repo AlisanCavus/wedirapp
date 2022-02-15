39961ce0760c4f62627caa58d179210419256ae9
AlisanCavus <cavusalisan@gmail.com>
2022-02-15 11:43:37 +0100

    slls

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@
 function App() {
   return (
     <div >
-     hello
+      <h1 className="animate__animated animate__bounce flex justify-center flex-col h-screen text-center align-middle text-6xl">  hello</h1>
     </div>
   );
 }
