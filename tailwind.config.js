9161763fe1ef258eebc608fe1e33bfa6a31f18f0
AlisanCavus <cavusalisan@gmail.com>
2022-03-09 15:59:36 +0100

    jjj

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
+  darkMode: false,
   theme: {
+    fontFamily: {
+      sans: "'Smooch Sans', 'sans-serif'"
+    },
     extend: {},
   },
   plugins: [],
