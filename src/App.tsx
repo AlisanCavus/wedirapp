2d04cd5e4b747f1ba6cd8c909fec0dbf95d28ae0
AlisanCavus <cavusalisan@gmail.com>
2022-03-15 13:23:36 +0100

    lsl

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import React from "react";
-
+import React, { lazy, Suspense } from 'react';
+const Loading = lazy(() => import('./Components/Loading'));
 
 function App() {
-
-  
   return (
-    <div className=" w-screen h-screen bg-slate-200">
-      hello
-    </div>
+    
+      <div className=" w-screen h-screen bg-slate-200">
+        <Suspense fallback={<Loading/>}>
+          Hello
+        </Suspense>
+          </div>
+   
   );
 }
 
