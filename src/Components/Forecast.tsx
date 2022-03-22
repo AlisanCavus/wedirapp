35703bf14560f4efdc2123bb6d8f0eabc27c65a2
AlisanCavus <cavusalisan@gmail.com>
2022-03-22 18:19:11 +0100

    jskls

--- src/Components/Forecast.tsx
+++ src/Components/Forecast.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Forecast() {
   return (
-    <div className="w-1/2 h-5/6 flex justify-center">
+    <div className="w-1/2 h-5/6 flex justify-center relative">
         <span className="text-2xl">Forecast</span>
     </div>
   )
