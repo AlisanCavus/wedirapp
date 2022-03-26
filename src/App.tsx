d156f9e188c8905d35ab2e3cf6ed67af5bbff195
AlisanCavus <cavusalisan@gmail.com>
2022-03-26 21:58:43 +0100

    loading

--- src/App.tsx
+++ src/App.tsx
@@ -6,28 +6,41 @@ import Forecast from './Components/Forecast';
 import Main from './Components/Main'
 
 
+
 const App: React.FC = () => {
 
- const {error, isLoading, forecast, submitRequest ,setError} = useForecast()
+ const {error, isLoading, forecast, submitRequest ,setError } = useForecast()
 
  const submitSearch = (value:string) => {
-  
-   submitRequest(value)
+    
+    submitRequest(value)
  }
+
 
  const errorHandler = () => {
    setError(false)
  }
-  return (
+
+
+  
       
-      <div className=" min-w-screen min-h-screen bg-wedir bg-cover bg-no-repeat flex justify-center flex-col">
-       {!isLoading && <Form submitSearch={submitSearch} />}
-       {error && <Error errorHandler={errorHandler}/>}
-       {isLoading && <Loader />}
-       {forecast ? <Forecast forecast={forecast} /> : <Main />}
-      </div>
-   
-  );
+      
+        return (
+        
+        
+        <div className="min-w-screen min-h-screen bg-wedir bg-no-repeat flex justify-center flex-col"> 
+        <Form submitSearch={submitSearch}/>
+        {error && <Error errorHandler={errorHandler}/>}
+        {isLoading && <Loader />}
+        {forecast ? <Forecast forecast={forecast} /> : <Main />}
+       </div>
+       
+       
+       ) 
+      
+      
+  
+  
 }
 
 export default App;
--- src/Components/Loader.tsx
+++ src/Components/Loader.tsx
@@ -2,23 +2,67 @@ import React from 'react';
 
 function Loader() {
   return (
-    <div className="w-screen h-screen bg-slate-400 flex justify-center flex-col items-center ">
-      <svg
-        role="status"
-        className="mr-2 w-20 h-20 text-white animate-spin dark:text-white fill-blue-800"
-        viewBox="0 0 100 101"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-          fill="currentColor"
-        />
-        <path
-          d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-          fill="currentFill"
-        />
-      </svg>
+    <div className="min-w-screen min-h-[screen] bg-wedir bg-no-repeat flex justify-center">
+      <div className="w-[35vw] h-[70vh] flex flex-col justify-start items-center text-center m-auto bg-orange-50 rounded-xl xs:rounded-none lg:w-1/2 xl:w-1/2 xs:w-full xs:h-full sm:w-11/12 sm:h-full md:w-5/6 ">
+      <div className="flex w-full h-1/12 justify-around mx-auto items-center my-4">
+        <div className=" min-w-1/2 flex justify-center">
+          <div className="w-72 h-8 bg-slate-300 animate-pulse rounded-xl"></div>
+        </div>
+      </div>
+      <div className="flex w-full h-1/4 justify-around mx-auto items-center ">
+        <div className="w-1/2">
+          <div className="w-40 h-36 bg-slate-300 animate-pulse rounded-xl mx-auto"></div>
+        </div>
+        <div className="w-1/2 h-full flex flex-col justify-center items-center">
+          <div className="w-40 h-36 bg-slate-300 animate-pulse rounded-xl mx-auto"></div>
+        </div>
+      </div>
+
+      <div className="flex w-full h-1/6 justify-around mx-auto items-center ">
+        <div className=" flex w-1/2 ml-10 justify-center sm:ml-1 ">
+          <div className="w-40 h-8 bg-slate-300 animate-pulse rounded-xl mx-auto"></div>
+        </div>
+        <div className=" flex w-1/2 mr-10 justify-center sm:mr-1 ">  
+          <div className="w-40 h-8 bg-slate-300 animate-pulse rounded-xl mx-auto"></div>
+        </div>
+      </div>
+
+      <div className="flex w-full h-1/6 justify-around mx-auto items-center sm:my-4">
+        <div className=" flex w-1/3 justify-center">
+          <div className="w-32 h-8 bg-slate-300 animate-pulse rounded-xl mx-auto"></div>
+        </div>
+        <div className=" flex w-1/3 justify-center">
+          <div className="w-32 h-8 bg-slate-300 animate-pulse rounded-xl mx-auto"></div>
+        </div>
+        <div className=" flex w-1/3 justify-center">
+          <div className="w-32 h-8 bg-slate-300 animate-pulse rounded-xl mx-auto"></div>
+        </div>
+
+      </div>
+
+      <div className="flex w-full h-1/3 flex-col lg:h-1/4 items-center">
+        
+        <div className="w-60 h-32 bg-slate-300 animate-pulse rounded-xl mx-auto"></div>
+
+          <div className="w-full flex h-1/4 justify-evenly items-center">
+            <div className=" flex flex-col justify-center items-center sm:w-1/3 ">
+              <div className="w-32 h-8 bg-slate-300 animate-pulse rounded-xl mx-auto"></div>
+            </div>
+            <div className=" flex flex-col justify-center items-center sm:w-1/3 ">
+              <div className="w-32 h-8 bg-slate-300 animate-pulse rounded-xl mx-auto"></div>
+            </div>
+          </div>
+         
+      </div>
+
+      <div className="w-full h-1/10 my-5 flex justify-center items-center">
+        <div className="w-60 h-8 bg-slate-300 animate-pulse rounded-xl mx-auto"></div>
+      </div>
+
+      <div className="w-full h-1/10 mb-5 flex justify-center items-center">
+        <div className="w-32 h-8 bg-slate-300 animate-pulse rounded-xl mx-auto"></div>
+      </div>
+    </div>
     </div>
   );
 }
