a9a0d27746d8d60f1c8b72bcb10bfe2c5f83aa87
AlisanCavus <cavusalisan@gmail.com>
2022-03-27 00:50:51 +0100

    typo

--- src/Components/Main.tsx
+++ src/Components/Main.tsx
@@ -16,7 +16,7 @@ const Main: any = ( {isLoading, error, errorHandler}: bo) => {
   if (!isLoading && !error) {
     return (<div className="w-[35vw] h-[70vh] flex justify-start items-center text-center m-auto bg-orange-50 rounded-xl xs:rounded-none lg:w-1/2 xl:w-1/2 xs:w-full sm:w-11/12  md:w-5/6 ">
     <div className="flex justify-center items-center text-center mx-auto xs:w-full sm:w-11/12  ">
-        Welcome to the WerdiApp!
+        Welcome to the WedirApp!
     </div>
 </div>)
   } else if (isLoading) {
