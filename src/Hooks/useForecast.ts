4f087e3b4432792baa847034d409c19a971b5af5
AlisanCavus <cavusalisan@gmail.com>
2022-04-06 13:36:57 +0200

    hlh

--- src/Hooks/useForecast.ts
+++ src/Hooks/useForecast.ts
@@ -1,6 +1,6 @@
 import {useState } from 'react'
 import axios from 'axios'
-// const BASE_UNSPLASH = `https://api.unsplash.com/search/photos?page=1&client_id=tlTgLcP_PUOS5LYj0jfYQmFbRSwp_xnegEJ7UDaZErI&query=`
+
 const BASE_URL: string = `https://api.openweathermap.org/data/2.5/weather?&appid=${process.env.REACT_APP_OPENWEATHER_API_KEY}&units=metric&q=`
 const useForecast = () => {
     const [ error, setError ] = useState<boolean>(false)
@@ -24,17 +24,7 @@ const useForecast = () => {
         }
     }
 
-    // const submitBg = async (location:string) => {
-    //     const { data } = await axios(`${BASE_UNSPLASH}${location}`)
 
-    //     if (!data || data.lenght === 0) {
-    //         setError(true)
-    //         return
-    //     } else {
-    //         setIsLoading(false)
-    //         setBackground(data.results[0])
-    //     }
-    // }
 
    
     return {
